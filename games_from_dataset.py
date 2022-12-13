3f81ee401641929a202c2682cda931127a65d84f
Riccardo Sepe <seric814@gmail.com>
2022-12-13 11:48:51 +0100

    Fixed documentation

--- games_from_dataset.py
+++ games_from_dataset.py
@@ -9,7 +9,9 @@ def file_parser(fname: str = FILENAME) -> chess.pgn.Game:
     This function yields one game at a time, taken from the file @fname.
 
     :param fname: the name of the pgn dataset file
+    :type fname: str
     :return: one game at a time
+    :rtype: chess.pgn.Game
     """
     with open(fname) as f:
         while True:
@@ -28,7 +30,9 @@ def game_states(game: chess.pgn.Game) -> tuple[tuple[str, str], str]:
     This function yields one tuple at a time in the form (s_t, s_t+1)
 
     :param game: the game taken from the database (class chess.pgn.Game)
+    :type game: chess.pgn.Game
     :return: the tuple (s_t, s_t+1) and the ground truth (the move that led from s_t to s_t+1)
+    :rtype: tuple
     """
     # create a new board
     board = chess.Board()

e1bb19f1acd835873975649cfb55d9cc54bf147f
Marco Destradis <marcodestradis12@gmail.com>
2023-04-16 17:34:27 +0200

    Changes by riccardo

--- games_from_dataset.py
+++ games_from_dataset.py
@@ -21,7 +21,6 @@ DICTIONARY = {
     'K': 12,
 }
 
-REVERSE_DICTIONARY = {v: k for k, v in DICTIONARY.items()}
 
 TURN_DICTIONARY = {'b': 0, 'w': 1}
 REVERSE_TURN_DICTIONARY = {0: 'b', 1: 'w'}
@@ -76,115 +75,6 @@ def game_states(game: chess.pgn.Game) -> tuple[tuple[str, str], str]:
         board.push(move)
         # yield the result one at a time
         yield (old_board, board), move.uci()
-
-
-def board_to_array(board: chess.Board) -> np.ndarray:
-    cells = str(board).split()
-    # flip each row to restore the correct cell ordering
-    for i in range(8):
-        cells[i * 8:i * 8 + 8] = cells[i * 8:i * 8 + 8][::-1]
-
-    cells_encoding = np.array(list(map(lambda x: DICTIONARY[x], reversed(cells))))
-    castling = str(board.fen()).split()[2]
-
-    if 'k' in castling:
-        cells_encoding[63] += OFFSET_CASTLING
-    if 'q' in castling:
-        cells_encoding[56] += OFFSET_CASTLING
-    if 'K' in castling:
-        cells_encoding[7] += OFFSET_CASTLING
-    if 'Q' in castling:
-        cells_encoding[0] += OFFSET_CASTLING
-
-    if board.ep_square is not None:
-        # compute offset for target ep square based on current player
-        s = 1 if board.ep_square < 32 else -1
-
-        cells_encoding[board.ep_square + s * 8] += OFFSET_ENPASSANT
-
-    # True if w, False if b
-    turn = str(board.fen()).split()[1]
-    half_move = str(board.fen()).split()[4]
-    full_move = str(board.fen()).split()[5]
-
-    cells_encoding = np.concatenate((cells_encoding, np.array([TURN_DICTIONARY[turn], int(half_move), int(full_move)])),
-                                    dtype=int)
-
-    return cells_encoding
-
-
-def array_to_board(array: np.ndarray) -> chess.Board:
-    castling = ""
-    enpassant = None
-
-    for i in range(64):
-        if 20 < array[i] < 100:
-            array[i] = array[i] - 20
-            castling += CASTLING_INDICES[i]
-
-        if array[i] > 100:
-            array[i] = array[i] - 100
-            s = 1 if i < 32 else -1
-            enpassant = i - s * 8
-
-    cells_dict = {i: chess.Piece(*v) for i, v in enumerate(
-        map(lambda x: (x, False)
-            if x <= OFFSET_COLOR
-            else (x - OFFSET_COLOR, True), array[:64]))}
-
-    board = chess.Board()
-    board.set_piece_map(cells_dict)
-    board.set_castling_fen(''.join(sorted(castling)))
-    board.ep_square = enpassant
-
-    board.turn = bool(array[64])
-    board.halfmove_clock = array[65]
-    board.fullmove_number = array[66]
-
-    return board
-
-
-def base_test():
-    fen_white = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
-    fen_black = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-    fen_ep = "rnbqkbnr/ppp2ppp/8/3p4/4pP2/8/PPPP2PP/RNBQKBNR b KQkq f3 0 2"
-
-    board = chess.Board(fen=fen_ep)
-    # print(board.fen())
-    fen1 = board.fen()
-    array = board_to_array(board)
-    # print(array)
-
-    board2 = array_to_board(array)
-    # print(board2.fen())
-    fen2 = board2.fen()
-
-    print(fen1 == fen2)
-
-
-def main():
-    # iterate through each game from the dataset
-    for i, game in enumerate(file_parser()):
-
-        print(f"******************GAME {i + 1}******************")
-
-        # iterate through states tuples
-        for (s1, s2), l in game_states(game):
-            fen1 = s1.fen()
-            array = board_to_array(s1)
-            b2 = array_to_board(array)
-            fen2 = b2.fen()
-            c = (fen1 == fen2)
-            if not c:
-                print(s1)
-                print(fen1)
-                print(b2)
-                print(fen2)
-                exit(1)
-
-        # for debug purpose, stop at first iteration
-        if i == 0:
-            pass
 
 
 if __name__ == '__main__':

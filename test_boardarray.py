d0c2542fb50fdbb0043a62232bcad14bcfa81e4e
Marco Destradis <marcodestradis12@gmail.com>
2023-04-17 11:53:15 +0200

    added test for BoardArray

--- test_boardarray.py
+++ test_boardarray.py
@@ -1,10 +1,33 @@
 from unittest import TestCase
-
+import chess
+import numpy as np
 from games_from_dataset import file_parser, game_states
 import boardarray
 
 
 class TestBoardArray(TestCase):
+
+    def test_to_board(self):
+        array = np.array([30, 0, 9, 11, 12, 9, 0, 30, 7, 7, 0, 0, 0, 0, 7, 7, 0, 0, 8,
+                          0, 0, 8, 0, 0, 0, 0, 7, 0, 7, 7, 0, 0, 0, 0, 1, 7, 101, 0,
+                          0, 0, 0, 0, 0, 1, 0, 2, 1, 0, 1, 1, 0, 0, 0, 1, 3, 1, 4,
+                          2, 3, 5, 0, 4, 6, 0, 1, 0, 8])
+        fen = "rnbq1rk1/pp3pbp/3p1np1/2pPp3/2P1PP2/2N2N2/PP4PP/R1BQKB1R w KQ e6 0 8"
+        b1 = chess.Board(fen=fen)
+        b2 = boardarray.BoardArray(array=array)
+        self.assertEqual(b1, b2)
+
+    def test_to_array(self):
+        array = np.array([30, 0, 9, 11, 12, 9, 0, 30, 7, 7, 7, 0, 0, 7, 7, 7, 0, 0,
+                          8, 0, 0, 8, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 1, 0, 101, 7,
+                          0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 3, 1,
+                          24, 2, 3, 5, 6, 0, 2, 24, 1, 0, 6])
+        fen = "rnbqk1nr/p3ppbp/2p3p1/1p1pP3/3P4/2N2N2/PPP2PPP/R1BQKB1R w KQkq d6 0 6"
+        b1 = boardarray.BoardArray(fen=fen)
+        b2 = b1.to_array()
+        print(b2)
+        print(array)
+        self.assertEqual(array.tolist(), b2.tolist())
 
     def test_board_to_array(self):
         # iterate through each game from the dataset

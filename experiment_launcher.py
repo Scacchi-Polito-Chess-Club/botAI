03e7b1dcc4c9248c9eba5ba2d53cde4b0d9a3f03
Paolo Rizzo <paolo.rizzo@studenti.polito.it>
2023-04-29 17:39:57 +0200

    fix: re-organized files and added parser

--- experiment_launcher.py
+++ experiment_launcher.py
@@ -1,42 +1,7 @@
-import argparse
-
 import torch.optim
-
 import games_from_dataset as gd
 import torch.nn as nn
 import torch.utils.data as data
-
-from utils.utils_autoencoder import get_autoencoder
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--resume', type=str, default=None, help='checkpoint path')
-    parser.add_argument('--device', type=str, default='cpu', help='device where to run model, cpu or cuda')
-    parser.add_argument('--wandb', type=str, default=None, help='wandb_logging')
-
-    # EXPERIMENT ARGS
-    parser.add_argument('--exp', type=str, default='train', help='type of experiment, train or test')
-    parser.add_argument('--epoch', type=int, default=50, help='number of epochs')
-    parser.add_argument('--optimizer', type=str, default='sgd', help='type of optimizer')
-    parser.add_argument('--scheduler', type=str, default='exp', help='type of scheduler')
-    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
-    parser.add_argument('--eval_step', type=int, default=10, help='rate of evaluation steps')
-
-    # DATA ARGS
-    parser.add_argument('--data', type=str, default='./dataset.pgn', help='path to dataset')
-    parser.add_argument('--n_games', type=int, default=10, help='number of games to be loaded')
-
-    # DATALOADER ARGS
-    parser.add_argument('--num_workers', type=int, default=0, help='number of workers for dataloader')
-    parser.add_argument('--bs', type=int, default=8, help='batch size')
-
-    # ENCODER ARGS
-    parser.add_argument('--encoder', type=str, default='conv', help='type of encoder')
-    parser.add_argument('--decoder', type=str, default='linear', help='type of decoder')
-    parser.add_argument('--latent_dim', type=int, default=16, help='dimension of the latent space')
-
-    return parser.parse_args()
 
 
 def get_optimizer(model: nn.Module, type: str, lr: float):
@@ -63,8 +28,8 @@ def get_critereon():
 
 
 @torch.no_grad()
-def test(model, test_data, args):
-    device = args.device
+def test(model, test_data, config):
+    device = config['setup_args']['device']
     eval_critereon = get_critereon(type)
     tot_stat = 0
     for (b1, b2), move_gt in test_data:
@@ -75,18 +40,18 @@ def test(model, test_data, args):
     return tot_stat
 
 
-def train(model: nn.Module, train_data: data.DataLoader, val_data: data.DataLoader, args):
-    device = args.device
+def train(model: nn.Module, train_data: data.DataLoader, val_data: data.DataLoader, config):
+    device = config['setup_args']['device']
     model = model.to(device)
-    optim = get_optimizer(model, args.optimizer, args.lr)
-    sched = get_scheduler(optim, type=args.scheduler)
+    optim = get_optimizer(model, config['exp_args']['optimizer'], config['exp_args']['lr'])
+    sched = get_scheduler(optim, type=config['exp_args']['scheduler'])
     critereon = get_critereon()
 
-    if args.resume is not None:
+    if config['setup_args']['resume'] is not None:
         raise NotImplementedError()
 
     init_epoch = 0
-    for epoch in range(init_epoch, args.epoch):
+    for epoch in range(init_epoch, config['exp_args']['epoch']):
         for (b1, b2), move_gt in train_data:
             b1, b2 = b1.to(device).float(), b2.to(device).float()
             optim.zero_grad()
@@ -103,30 +68,3 @@ def train(model: nn.Module, train_data: data.DataLoader, val_data: data.DataLoad
     return model
 
 
-def main(args):
-    """
-    Start a training or experiment run
-    :param args: arguments from argparse
-    :return: None
-    """
-
-    if args.wandb is not None:
-        raise NotImplementedError('logging not implemented yet')
-    model = get_autoencoder(args.encoder, args.decoder, args.latent_dim)
-    train_data, val_data, test_data = gd.get_dataloader(fname=args.data,
-                                                        num_workers=args.num_workers,
-                                                        batch_size=args.bs,
-                                                        board_transform=gd.board_to_array2)
-
-    if args.exp == 'train':
-        train(model, train_data, val_data, args)
-        test(model)
-    elif args.exp == 'test':
-        ckpt = torch.load(args.resume)
-        model.load_state_dict(ckpt['model_state_dict'], strict=True)
-        test(model)
-
-
-if __name__ == '__main__':
-    arguments = parse_arguments()
-    main(arguments)
--- models/autoencoder.py
+++ models/autoencoder.py
@@ -2,40 +2,49 @@ import torch.nn as nn
 import torch
 
 
-# TODO REDEFINE MODEL THIS IS A PLACEHOLDER
 # TODO DEFINE DECODER OUTPUT
 
 
 class AutoEncoder(nn.Module):
-    """
-    Definition of a custom autoencoder, with encoder and decoder.
-    :param in_dim: input dimension of the chessboard + additional information
-    :param out_dim: output dimension of action space
-    :param latent_dim: dimension of encoded features
-    """
 
-    def __init__(self, in_dim=67, out_dim=64, encoder='linear', decoder='linear', latent_dim=32):
+    def __init__(self, config: dict):
         super().__init__()
-        if encoder == 'linear':
-            # TODO: check dimensions
-            self.encoder = nn.Sequential(nn.Linear(in_dim, 256), nn.ReLU(), nn.Linear(256, 128), nn.ReLU(),
-                                         nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, latent_dim)
-                                         )
-        elif enconder == 'conv':
-            self.encoder = nn.Sequential(nn.Conv2d(1, 16, 2, 1), nn.ReLU(),  # 7x7
-                                         nn.Conv2d(16, 32, 3, 1), nn.ReLU(),  # 5x5
-                                         nn.Conv2d(32, latent_dim, 1, 1),  # 5x5
-                                         nn.AvgPool2d(5)  # 32x1 (check hxw)
-                                         )
-        else:
-            raise NotImplementedError(f'{encoder} encoder not implemented')
+        self.in_dim = config['encoder']['input_dim']
+        self.lat_dim = config['encoder']['laten_dim']
+        self.out_dim = config['decoder']['output_dim']
+        self.encoder = self.build_encoder(config)
+        self.decoder = self.build_decoder(config)
 
-        if decoder == 'linear':
-            self.decoder = nn.Sequential(nn.Linear(latent_dim, latent_dim * 2), nn.ReLU(),
-                                         nn.Linear(latent_dim * 2, out_dim)
-                                         )
+    def build_encoder(self, config):
+        if config['encoder']['type'] == 'linear':
+            # building encoder with Linear layers; it returns a number of features equal to lat_dim
+            encoder = nn.Sequential(nn.Linear(self.in_dim, 256), nn.ReLU(), nn.Linear(256, 128), nn.ReLU(),
+                                    nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, self.lat_dim)
+                                    )
+        elif config['encoder']['type'] == 'conv':
+            # TODO: check dimensions of the matrix
+            # building encoder with Convolutional Layers; THE INPUT MUST BE A MATRIX
+            encoder = nn.Sequential(nn.Conv2d(1, 16, 2, 1), nn.ReLU(),  # 7x7
+                                    nn.Conv2d(16, 32, 3, 1), nn.ReLU(),  # 5x5
+                                    nn.Conv2d(32, self.lat_dim, 1, 1),  # 5x5
+                                    nn.AvgPool2d(5)  # 32x1 (check hxw)
+                                    )
         else:
-            raise NotImplementedError(f'{encoder} encoder not implemented')
+            raise NotImplementedError(f'Encoder not implemented')
+
+        return encoder
+
+    def build_decoder(self, config):
+        if config['decoder']['type'] == 'linear':
+            # TODO: check action_space dimensions
+            # building decoder with Linear Layers; it returns a number of moves equal to out_dim
+            decoder = nn.Sequential(nn.Linear(self.lat_dim, self.lat_dim * 2), nn.ReLU(),
+                                    nn.Linear(self.lat_dim * 2, self.out_dim)
+                                    )
+        else:
+            raise NotImplementedError(f'Decoder not implemented')
+
+        return decoder
 
     def forward(self, b1, b2):
         """
@@ -48,4 +57,3 @@ class AutoEncoder(nn.Module):
         action_space = self.decoder(z3)
         assert action_space.shape == (batch_size, self.out_dim)
         return action_space
-
